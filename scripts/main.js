87d745a81aac1714902827ee7f1fbfc02ec275a8
Emily Bond <emilymbond@yahoo.com>
2014-07-17 02:26:22 -0400

    added templating

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,39 @@
-console.log('The Iron Yard Rocks');
+//Model
+var Person = Backbone.Model.extend ({
+  defaults: {
+    name: '',
+    bio: '',
+    imgUrl: ''
+  }
+});
+
+var person = new Person();
+
+//Collection
+var People = Backbone.Collection.extend ({
+  model: person
+  url: '/list'
+});
+
+var people = new People([
+  {
+    name: 'Amy Gori',
+    bio: '',
+    ImgUrl: ''
+  },
+  {
+    name: 'Talal Obeid',
+    bio: '',
+    ImgUrl: ''
+  },
+  {
+    name: 'Tyler Berry',
+    bio: '',
+    ImgUrl: ''
+  }
+]);
+
+//View
+var PeopleView = Backbone.Collection.extend ({
+  model:
+});
